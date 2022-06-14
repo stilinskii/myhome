12e2929ba93009a852cda47779d5f3fff1278fd4
stilinskii <lhy1031hy@gmail.com>
2022-06-15 00:18:32 +0900

    add login page / start to use spring security

---
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.godcoder.myhome.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+    //Authentication - 인증 로그인 관한 처리
+    //Authorization - 권한 설정 로그인 사용자의 권한 설정
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password,enabled "
+                        + "from user "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery("select u.username,r.name "
+                        + "from user_role ur inner join user n on ur.user_id = u.id "
+                        + "inner join role r on ur.role_id = r.id "
+                        + "where u.username = ?");
+    }
+    //비밀번호 암호화
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
